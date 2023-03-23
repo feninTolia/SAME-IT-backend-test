a4c8febd7608fe66cb27641414e61784b1fc8c92
feninTolia <fenintolja@gmail.com>
2023-03-23 10:38:52 +0200

    app setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function start() {
+  const PORT = process.env.PORT || 3001;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('SAME IT backend')
+    .setDescription('REST API docs')
+    .setVersion('1.0.0')
+    .addTag('Fenin')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/api/docs', app, document);
+
+  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
-bootstrap();
+
+start();
