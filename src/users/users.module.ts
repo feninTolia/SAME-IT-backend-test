b96fe159f8283f155a6ce39a742db56d43b59339
feninTolia <fenintolja@gmail.com>
2023-03-23 10:43:23 +0200

    users module

---
+++ src/users/users.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+import { Profile } from 'src/profiles/profiles.model';
+import { ProfilesModule } from 'src/profiles/profiles.module';
+import { UsersController } from './users.controller';
+import { User } from './users.model';
+import { UsersService } from './users.service';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService],
+  imports: [SequelizeModule.forFeature([User, Profile]), ProfilesModule],
+  exports: [UsersService],
+})
+export class UsersModule {}
